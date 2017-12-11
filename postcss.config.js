0bec4bf71cc6d25cc34d78da7f9b7c7126e101ad
Martín González <m@martingonzalez.net>
2017-12-11 21:54:50 +0000

    Initial setup

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+const pCSSNext = require("postcss-cssnext")({ warnForDuplicates: false });
+const pCSSImport = require("postcss-import");
+const pCSSNested = require("postcss-nested");
+
+module.exports = {
+  plugins: [pCSSImport, pCSSNext, pCSSNested]
+};
