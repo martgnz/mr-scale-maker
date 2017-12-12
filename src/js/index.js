6e13132fec47f5c98d5f59b4ce3c6c93529a40a2
Martín González <m@martingonzalez.net>
2017-12-12 21:39:42 +0000

    Display dropped data

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,45 @@
 import { select, event } from 'd3-selection';
 import { csvParse } from 'd3-dsv';
-import sampleData from './sample';
+// import sampleData from './sample';
 
-const data = csvParse(sampleData);
+// const data = csvParse(sampleData)
 const app = select('#app');
 const form = app.select('.data-input form');
+const output = app.select('.data-output');
 
 form
-  .on('drag dragstart dragend dragover dragenter dragleave drop', () => {
-    event.preventDefault();
-    event.stopPropagation();
-  })
+  .on(
+    'drag dragstart dragend.default dragover.default dragenter.default dragleave.default drop.default',
+    () => {
+      event.preventDefault();
+      event.stopPropagation();
+    },
+  )
   .on('dragover dragenter', () => {
     form.classed('is-dragover', true);
   })
   .on('dragleave dragend drop', () => {
     form.classed('is-dragover', false);
   })
-  .on('drop', () => {
-    const droppedFiles = event.originalEvent.dataTransfer.files;
-    console.log(droppedFiles);
-  });
+  .on('drop', dropped);
 
-const div = app
-  .select('.data-output')
-  .selectAll('div')
-  .data(data)
-  .enter();
+function dropped() {
+  const file = event.dataTransfer.files[0];
+  const reader = new FileReader();
 
-div.append('div').html(d => `${d.id}: ${d.value}`);
+  if (file.type.match('text.*')) {
+    reader.readAsText(file);
+
+    reader.onload = () => {
+      displayFile(reader.result);
+    };
+  }
+
+  form.classed('is-dragover', false);
+}
+
+function displayFile(input) {
+  const data = csvParse(input);
+
+  output.text(JSON.stringify(data));
+}
