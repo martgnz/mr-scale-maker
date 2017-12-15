51d23b5fe21419477d3a514b37f3de056f35a9a2
Martín González <m@martingonzalez.net>
2017-12-15 08:39:07 +0000

    Fix the quantile scale

--- src/js/compute-breaks.js
+++ src/js/compute-breaks.js
@@ -1,12 +1,11 @@
 import { scaleQuantile } from 'd3-scale';
-import { extent } from 'd3-array';
 
 export default function(data, column) {
   const breaks = {};
 
   const quantiles = scaleQuantile()
     .range([0, 1, 2, 3, 4])
-    .domain(extent(data, d => d[column]))
+    .domain(data.map(d => d[column]))
     .quantiles();
 
   breaks.quantiles = quantiles;
