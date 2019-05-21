b9f6c98150f2e07ecaf9684f53e691dc5094c985
Martín González <m@martingonzalez.net>
2019-05-22 00:27:50 +0100

    Update packages

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,18 @@
-const pCSSNext = require('postcss-cssnext')({ warnForDuplicates: false });
+/* eslint-disable import/no-extraneous-dependencies  */
+const pCSSEnv = require('postcss-preset-env');
 const pCSSImport = require('postcss-import');
 const pCSSNested = require('postcss-nested');
 
 module.exports = {
-  plugins: [pCSSImport, pCSSNext, pCSSNested],
+  plugins: [
+    pCSSImport,
+    pCSSEnv({
+      features: {
+        'custom-properties': {
+          preserve: false,
+        },
+      },
+    }),
+    pCSSNested,
+  ],
 };
