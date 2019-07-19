1c53f3dfc63648e7ee1f966827962cbf197206af
Luis Sevillano <luissevillanopires@gmail.com>
2019-07-19 12:34:15 +0200

    Add ckmeans distribution

--- src/js/compute-breaks.js
+++ src/js/compute-breaks.js
@@ -1,6 +1,6 @@
 import { scaleQuantile } from 'd3-scale';
 import { range, median, mean } from 'd3-array';
-import { equalIntervalBreaks  } from 'simple-statistics'
+import { equalIntervalBreaks, ckmeans  } from 'simple-statistics'
 
 export default function(classes, data, column) {
   const breaks = {};
@@ -13,9 +13,13 @@ export default function(classes, data, column) {
     .quantiles();
 
   const equalBreaks = equalIntervalBreaks(mappedData, classes - 1);
+  const ckmeansBreaks = ckmeans(mappedData, classes);
 
   const medians = median(mappedData);
   const means = mean(mappedData);
+
+  // https://github.com/simple-statistics/simple-statistics/blob/4db0dd820ebb5bc9bd7635715a3ef8a4678e180e/CHANGELOG.md#jenks---ckmeans
+  breaks.ckmeans = ckmeansBreaks.map(function(cluster) {return cluster[0];});
 
   breaks['equal breaks'] = equalBreaks;
   breaks.quantiles = quantiles;
