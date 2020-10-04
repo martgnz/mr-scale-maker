6707a08b6121f2181345cb4c18c2639dcd732071
Martín González <m@martingonzalez.net>
2020-10-04 21:36:25 +0100

    Comma

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -15,12 +15,11 @@
     Want to dig deeper? Read
     <a
       href="https://observablehq.com/@d3/quantile-quantize-and-threshold-scales"
-    >Quantile, Quantize and Threshold scales</a>
-    ,
+    >Quantile, Quantize and Threshold scales</a>,
     <a href="https://roadtolarissa.com/coloring-maps/">Coloring Maps</a>
     and
     <a
       href="https://earthobservatory.nasa.gov/blogs/elegantfigures/2013/08/05/subtleties-of-color-part-1-of-6/"
-    >Subtleties of Color</a>
+    >Subtleties of Color</a>.
   </p>
 </footer>
