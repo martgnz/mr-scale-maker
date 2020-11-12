5cb8759b8e9b1793e198437ba176a05f484c8478
Martín González <m@martingonzalez.net>
2020-11-12 10:29:07 +0100

    Fix mobile z-index on file upload

--- src/Upload.svelte
+++ src/Upload.svelte
@@ -20,7 +20,7 @@
   .drag {
     text-align: center;
     outline: 1px dashed var(--grey-stroke);
-    font-size: 1.25rem;
+    font-size: 1rem;
     background-color: var(--grey-bg);
     position: relative;
     transition: outline-offset 0.15s ease-in-out, background-color 0.15s linear;
@@ -116,6 +116,9 @@
     position: absolute;
     z-index: -1;
   }
+  .drag-info {
+    z-index: 1;
+  }
   .drag-filename {
     pointer-events: none;
     font-weight: 500;
@@ -154,6 +157,12 @@
     bottom: -1rem;
     width: 140px;
     transform: rotate(-87deg);
+  }
+
+  @media (min-width: 600px) {
+    .drag {
+      font-size: 1.25rem;
+    }
   }
 </style>
 
@@ -282,7 +291,10 @@
 
                 <div>Drop your file</div>
               </div>
-              <div class="drag-select">or <span class="underline">select it</span></div>
+              <div class="drag-select">
+                or
+                <span class="underline">select it</span>
+              </div>
             </div>
             <div class="small-note">CSV and TSV files are supported</div>
           {:else if isUploaded}
@@ -307,7 +319,7 @@
                   <circle cx="10.5" cy="8" r="1.5" />
                 </g>
               </g></svg>
-            <div>
+            <div class="drag-info">
               <div class="drag-filename">{fileMetadata.name}</div>
               <div class="drag-filesize">{bytesToSize(fileMetadata.size)}</div>
             </div>
@@ -322,8 +334,10 @@
               <path
                 d="M4.59 6.89c.7-.71 1.4-1.35 1.71-1.22.5.2 0 1.03-.3 1.52-.25.42-2.86 3.89-2.86 6.31 0 1.28.48 2.34 1.34 2.98.75.56 1.74.73 2.64.46 1.07-.31 1.95-1.4 3.06-2.77 1.21-1.49 2.83-3.44 4.08-3.44 1.63 0 1.65 1.01 1.76 1.79-3.78.64-5.38 3.67-5.38 5.37 0 1.7 1.44 3.09 3.21 3.09 1.63 0 4.29-1.33 4.69-6.1H21v-2.5h-2.47c-.15-1.65-1.09-4.2-4.03-4.2-2.25 0-4.18 1.91-4.94 2.84-.58.73-2.06 2.48-2.29 2.72-.25.3-.68.84-1.11.84-.45 0-.72-.83-.36-1.92.35-1.09 1.4-2.86 1.85-3.52.78-1.14 1.3-1.92 1.3-3.28C8.95 3.69 7.31 3 6.44 3 5.12 3 3.97 4 3.72 4.25c-.36.36-.66.66-.88.93l1.75 1.71zm9.29 11.66c-.31 0-.74-.26-.74-.72 0-.6.73-2.2 2.87-2.76-.3 2.69-1.43 3.48-2.13 3.48z"
               /></svg>
-            <div class="drag-error-title">Error while reading the file</div>
-            <div class="drag-error-hint">please upload a valid CSV</div>
+            <div class="drag-info">
+              <div class="drag-error-title">Error while reading the file</div>
+              <div class="drag-error-hint">please upload a valid CSV</div>
+            </div>
           {/if}
         </label>
       </div>
